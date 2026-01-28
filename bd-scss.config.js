345dc2dd413ee643d0bc010802650e7bac557c17
jmclean21 <joshua.mclean697@gmail.com>
2026-01-28 14:10:39 +0100

    created theme

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Halcyon',
+		author: 'ilothepyro',
+		version: '1.0.0',
+		description: 'Restores discord to its original theme while also applying unrounding.',
+		source: 'https://github.com/IloThePyro/Halcyon'
+	}
+};
